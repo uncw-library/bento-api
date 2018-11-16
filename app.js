ae24bd5bc3a1ff2b760157479d1d7e9b783e151b
ElliotSimpson <simpsonj@uncw.edu>
2018-11-16 10:29:50 -0500

    remove unnecessary files/packages/config

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-require('marko/node-require'); // allow Node.js to require and load `.marko` files
+/**
+* our main app.js file
+* used for initializing our Express application
+*/
 
 const express = require('express');
-const markoExpress = require('marko/express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(markoExpress());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
+/**
+* our '/' routes
+*/
+
 const express = require('express');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.json({
+    name: 'Elliot',
+  });
 });
 
 module.exports = router;
