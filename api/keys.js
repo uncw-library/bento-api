b39f0f7023f597986365a0ad20f7f110147f1046
ElliotSimpson <simpsonj@uncw.edu>
2018-12-04 09:03:11 -0500

    use environment variables

--- api/keys.js
+++ api/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  sierra: 'YlhKb0lBR3I5NjhyU1IzMlcwdnNEa1dFS25YeTpjb3JiZXR0Z1NpZXJyYVNlY3JldA==',
-  browzine: 'b29847f0-ba9a-4af9-bfe6-18327a19f96e',
+  sierra: process.env.SIERRA_API_KEY,
+  browzine: process.env.BROWZINE_API_KEY,
 };
