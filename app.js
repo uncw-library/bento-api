9d9260ecedf0e3e1c16cab5a3ac6928337d517f2
ElliotSimpson <simpsonj@uncw.edu>
2018-12-04 09:19:14 -0500

    enable cors requests

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -15,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 
